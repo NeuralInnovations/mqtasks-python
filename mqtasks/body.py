26413bd8451d20f039c6c4c9232c60637f5cb3eb
dorofiykolya <dorofiy.kolya@gmail.com>
2023-10-04 09:49:34 +0100

    0.0.1-dev26

--- mqtasks/body.py
+++ mqtasks/body.py
@@ -1,19 +1,42 @@
 import json
+from typing import Any
 
 
 class MqTaskBody:
-    body: bytes
-    size: int
+    __json_cache: Any | None = None
+    __string_cache: str | None = None
+    __body: bytes
+    __size: int
 
     def __init__(self, body: bytes, size: int):
-        self.body = body
-        self.size = size
+        self.__body = body
+        self.__size = size
+
+    @property
+    def body(self) -> bytes:
+        return self.__body
+
+    @property
+    def size(self) -> int:
+        return self.__size
 
     def as_string(self):
-        return self.body.decode()
+        if self.__string_cache is None:
+            self.__string_cache = self.body.decode()
+        return self.__string_cache
+
+    @property
+    def text(self) -> str:
+        return self.as_string()
 
     def as_bytes(self):
         return self.body
 
     def as_json(self):
-        return json.loads(self.body.decode())
+        if self.__json_cache in None:
+            self.__json_cache = json.loads(self.as_string())
+        return self.__json_cache
+
+    @property
+    def json(self) -> Any:
+        return self.as_json()
